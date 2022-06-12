f210dd88905d6761b632a334ba1e8b9f5480ddea
davigon <dvgnlz@outlook.es>
2022-06-12 16:59:49 +0200

    Add Chakra UI

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { ChakraProvider } from "@chakra-ui/react"
 import { Route, Routes } from "react-router-dom"
 import { Footer } from "./components/Footer"
 import { Navbar } from "./components/Navbar"
@@ -7,15 +8,17 @@ import { NotFoundPage } from "./pages/NotFoundPage"
 
 export const App = () => {
   return (
-    <div className="App">
-      <Navbar />
-      <main className="Main">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </main>
-      <Footer />
-    </div>
+    <ChakraProvider>
+      <div className="App">
+        <Navbar />
+        <main className="Main">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </ChakraProvider>
   )
 }
