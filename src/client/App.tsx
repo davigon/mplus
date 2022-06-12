87d25e6804f32d1c539eb83481c3efe973c5ddb6
davigon <dvgnlz@outlook.es>
2022-06-12 17:04:30 +0200

    Add theme

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,14 +1,16 @@
 import React from "react"
-import { ChakraProvider } from "@chakra-ui/react"
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react"
 import { Route, Routes } from "react-router-dom"
 import { Footer } from "./components/Footer"
 import { Navbar } from "./components/Navbar"
 import { HomePage } from "./pages/HomePage"
 import { NotFoundPage } from "./pages/NotFoundPage"
+import theme from "./theme"
 
 export const App = () => {
   return (
     <ChakraProvider>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <div className="App">
         <Navbar />
         <main className="Main">
